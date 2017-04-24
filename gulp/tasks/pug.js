e4f38468d9019a05906da258411ecc4fb2a76a4e
Max Rysukhyn <m.mowgly@gmail.com>
2017-04-24 06:32:20 +0300

    finished about page

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 module.exports = function() {
   $.gulp.task('pug', function() {
     return $.gulp.src('./source/template/pages/*.pug')
-      .pipe($.gp.pug({ pretty: true }))
+      .pipe($.gp.pug({ locals: JSON.parse(fs.readFileSync('./source/data/content.json', 'utf-8')), pretty: true }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
           title: 'Pug',
