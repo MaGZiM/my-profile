b0206796ae72300cd1e3f484bc53892e597effed
Max Rysukhyn <m.mowgly@gmail.com>
2017-05-13 14:32:09 +0300

    used svg on portfolio page

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,22 @@
 (function() {
   'use strict';
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+
 })();
+
+function initMap() {
+  var myLatlng = new google.maps.LatLng(50.005, 36.229);
+  var myOptions = {
+    zoom: 12,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false,
+    mapTypeControl: false
+  }
+  var map = new google.maps.Map(document.getElementById("my-map"), myOptions);
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title:"Hello World!"
+  });
+};
