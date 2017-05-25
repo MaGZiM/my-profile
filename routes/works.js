97c74bc7bf553f52cd7009b2d88446f932c58d93
Max Rysukhyn <m.mowgly@gmail.com>
2017-05-25 23:56:49 +0300

    finished adaptive for index and works pages. Fixed hamburger

--- routes/works.js
+++ routes/works.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let obj = {
-    title: 'Мои работы'
+    title: 'Мои работы',
+    footerClass: 'footer_transparent'
   }
 
   Object.assign(obj, req.app.locals.settings);
